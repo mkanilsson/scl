5430e067ca1f80c82bd07d0cc168b258f951cbe0
Marcus Nilsson <marcus.nilsson@genarp.com>
2025-08-02 13:28:11 +0200

    lexer: Add support for comments

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,7 +1,7 @@
 use logos::{Logos, SpannedIter};
 
 #[derive(Logos, Debug, PartialEq, Clone)]
-#[logos(skip r"[ \t\n\f]+")]
+#[logos(skip r"[ \t\n\f]+", skip r"//.*\n")]
 pub enum Token<'source> {
     #[regex("-*[0-9]+")]
     Number(&'source str),
